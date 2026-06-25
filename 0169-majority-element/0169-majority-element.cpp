524e8a34c42888479ebd83e7b3a496d8cfb64622
Saransh-Saurav <saranshcollegebackup@gmail.com>
2026-06-25 15:40:43 +0530

    Time: 3 ms (27.96%), Space: 28.2 MB (28.35%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,18 @@
+//optimal solution-> moore's voting algo
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int el;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                cnt++;
+                el=nums[i];
+            }
+            else if(nums[i]==el)cnt++;
+            else cnt--;
+        }
+        return el;
+    }
+};
