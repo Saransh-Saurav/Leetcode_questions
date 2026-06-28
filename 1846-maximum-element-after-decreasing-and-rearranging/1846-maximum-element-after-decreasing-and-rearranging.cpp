c500737272291a1fc17e0dacac0650ab74a0899f
Saransh Saurav <saranshcollegebackup@gmail.com>
2026-06-28 11:05:01 +0530

    Time: 11 ms (41.24%), Space: 55.4 MB (8.47%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,7 +4,7 @@ public:
         sort(arr.begin(),arr.end());
         arr[0]=1;
         for(int i=1;i<arr.size();i++){
-            if(arr[i]>arr[i-1]+1)arr[i]=arr[i-1]+1;
+            arr[i]=min(arr[i],arr[i-1]+1);
         }
         return arr.back();
     }
