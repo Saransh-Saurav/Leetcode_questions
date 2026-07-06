c96811c563e082838cd24643d841156f79d11dff
Saransh Saurav <saranshcollegebackup@gmail.com>
2026-07-06 12:10:48 +0530

    Time: 3 ms (42.52%), Space: 15.2 MB (60.13%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    static bool SortingStyle(vector<int>&a, vector<int>&b){
+        if(a[0]==b[0]) return a[1]>b[1];
+        return a[0]<b[0];
+    }
+
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        int n=intervals.size();
-        int ans=intervals.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j) continue;
-                if(intervals[i][0]>=intervals[j][0] && intervals[i][1]<=intervals[j][1]){
-                    ans--;
-                    break;
-                }
-            }
+        int ans=0;
+        int maxEnd=0;
+        sort(intervals.begin(),intervals.end(),SortingStyle);
+        for(auto &i:intervals){
+            if(i[1]<=maxEnd) continue;
+            maxEnd=i[1];
+            ans++;
         }
 
         return ans;
