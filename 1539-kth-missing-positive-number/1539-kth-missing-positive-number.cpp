b615f3a1b3ef98169a4fcb27aae538baaea721ff
Saransh Saurav <saranshcollegebackup@gmail.com>
2026-07-09 13:40:29 +0530

    Time: 0 ms (100%), Space: 13.2 MB (92.94%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        for(int i:arr){
-            if(i<=k)k++;// as series is from 1 to n and for every value present in arr <=k, the search of element inc by 1,like in arr [5,7] the 5th missing alement is 6 ie 5+1
-            else break;
+        int low=0;
+        int high=arr.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int missing=arr[mid]-(mid+1);
+            if(missing<k)low=mid+1;
+            else high=mid-1;
         }
-        return k;
+        //at this point low>high and we got the interval in which kth missing element lies
+        //we have to return arr[high]+more-->arr[high]+(k-missing)
+        //but we have to tweek this because for case like [5,6,7] k=4 the answer should be 4 but high pointing to -1 gives error ans,so we have to eliminate arr[high]
+        // arr[high]+k-missing->(we can write missing=arr[high]-(high+1))
+        //so it becomes--> arr[high]+k-(arr[high]-high-1)-->k+high +1
+        //now high+1==low so we can write--> k+low;
+        return low+k;
     }
 };
