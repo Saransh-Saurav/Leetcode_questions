3b9a3c026a5ba0b06182ebd17bc9da641c3bf4c0
Saransh Saurav <saranshcollegebackup@gmail.com>
2026-08-18 15:34:58 +0530

    Time: 2 ms (56.33%), Space: 20.5 MB (17.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,12 @@
-#BRUTE FORCE
+#Better
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mpp={}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
-        return -1
+            remaining=target-nums[i]
+            if remaining in mpp:
+                return [mpp[remaining],i]
+            
+            mpp[nums[i]]=i
+
+        return []
